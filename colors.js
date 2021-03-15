e0c52703e63e4e8205df075316e99a4d7371ec72
Daniel McCord <git@danielmccord.dev>
2021-03-15 03:23:18 -0700

    Add colors.

---
+++ colors.js
@@ -0,0 +1,77 @@
+/**
+ * An object representing an RGB color with no transparency.
+ * A `name` for the color can be included.
+ */
+export class Color {
+    /**
+     * @param {string} [name]
+     * @param {number} r
+     * @param {number} g
+     * @param {number} b
+     */
+    constructor(name, r, g, b) {
+        this.name = name;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    get values() {
+        return [this.r, this.g, this.b];
+    }
+
+    /**
+     * Returns the color's 6-digit hexadecimal RGB code, prefixed with '#'.
+     * e.g. '#ff0000' for pure red.
+     */
+    get hexStr() {
+        return this.values.reduce((str, channel) => str.concat(
+            ((c) => {
+                const hex = Math.min(Math.max(c, 0), 255).toString(16);
+                return '0'.repeat(2 - hex.length).concat(hex);
+            })(channel)
+        ), '#');
+    }
+
+    isGrayscale() {
+        // for (let i = 1, v = this.values; i < v.length; i++) if (v[i-1] !== v[i]) return false;
+
+        // return true;
+        const v = this.values;
+        return v.reduce((acc, _, i) => acc && Number(v[i-1]) === Number(v[i]));
+    }
+}
+
+// These color codes are from
+// https://eleanormaclure.files.wordpress.com/2011/03/colour-coding.pdf
+// They are intended to be mostly easy to distinguish.
+// Each RGB triplet in this array corresponds to a letter of the alphabet,
+// i.e. [240, 163, 255] is 'A', [0, 117, 220] is 'B', ... , [255, 80, 5] is 'Z'.
+const colorCodesAlphabetical = [
+    [240, 163, 255], [0, 117, 220], [153, 63, 0], [76, 0, 92],
+    [25, 25, 25], [0, 92, 49], [43, 206, 72], [255, 204, 153],
+    [128, 128, 128], [148, 255, 181], [143, 124, 0], [157, 204, 0],
+    [194, 0, 136], [0, 51, 128], [255, 164, 5], [255, 168, 187],
+    [66, 102, 0], [255, 0, 16], [94, 241, 242], [0, 153, 143],
+    [224, 255, 102], [116, 10, 255], [153, 0, 0], [255, 255, 128],
+    [255, 255, 0], [255, 80, 5]
+];
+
+// My personal semi-arbitrary ordering of the colors above.
+// At least the first six colors are colorblind-safe,
+// such that any dichromatic human would still see differences between them.
+const customAlphabetOrder = [ 
+    'R', 'W', 'Y', 'S', 'B', 'D', 'P', 'N',
+    'M', 'V', 'Q', 'K', 'O', 'U', 'H', 'G', 'L', 'Z', 'C', 'A', 'I', 
+    'E', 'T', 'F', 'X', 'J'
+];
+
+/**
+ * An array of 26 colors with good contrast.
+ */
+export const colors = customAlphabetOrder.map(
+    (letter) => new Color(
+        letter,
+        ...colorCodesAlphabetical['ABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(letter)]
+    )
+);
